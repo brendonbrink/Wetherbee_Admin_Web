58b02df667a5edd325e0fc37a18305854e28f39f
Brendon_Brink <brendon.brink@gmail.com>
2020-01-21 21:56:29 +0100

    Updates

---
+++ precache-manifest.011751bc47c5eb1b85c59c3f6d3c19bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb94a6efc38c70bbe23a737bcb3fd67b",
+    "url": "/wetherbee-admin/index.html"
+  },
+  {
+    "revision": "327ebd9e8a1392074894",
+    "url": "/wetherbee-admin/static/css/2.88a9e19c.chunk.css"
+  },
+  {
+    "revision": "a2083e50836c6f992dbe",
+    "url": "/wetherbee-admin/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "327ebd9e8a1392074894",
+    "url": "/wetherbee-admin/static/js/2.83d80c1e.chunk.js"
+  },
+  {
+    "revision": "0bc7b1a32b50055ba5011d82f01aa9a5",
+    "url": "/wetherbee-admin/static/js/2.83d80c1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a2083e50836c6f992dbe",
+    "url": "/wetherbee-admin/static/js/main.5d7655ee.chunk.js"
+  },
+  {
+    "revision": "15594cc75c46e7845db0",
+    "url": "/wetherbee-admin/static/js/runtime-main.e539825c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wetherbee-admin/static/media/logo.5d5d9eef.svg"
+  }
+]);
